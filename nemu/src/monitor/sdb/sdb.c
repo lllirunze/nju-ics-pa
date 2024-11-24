ad90a9ca0288082d6f8e2ae8ae722568ef353f7c
tracer-ics2024 <tracer@njuics.org>
2024-11-23 23:27:41 -0500

    >  compile NEMU 3019244266 lirunze Linux ubuntu-vm 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:27:41 up  1:53,  1 user,  load average: 0.33, 0.38, 0.39

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -116,7 +116,7 @@ static int cmd_x(char *args) {
   }
   // printf("%s\n", expr);
   unsigned int addr = (unsigned int)strtoul(expr, NULL, 10);
-  printf("%x\n", addr);
+  printf("%08x\n", addr);
   // unsigned int desc = vaddr_read(addr, 4);
   // printf("0x%08x\t%x", addr, desc);
 
