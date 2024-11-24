0fc1d81a09ba6ef2fbe2bf6dc9a98f70318b81fc
tracer-ics2024 <tracer@njuics.org>
2024-11-23 20:55:50 -0500

    >  compile NEMU 3019244266 lirunze Linux ubuntu-vm 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:55:50 up 37 min,  1 user,  load average: 0.61, 0.51, 0.46

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -27,7 +27,7 @@ void isa_reg_display() {
   // printf("I want to print out riscv-32 registers.\n");
   int i;
   for (i = 0; i < 32; i++) {
-    printf("%d\t%u\n", i, gpr(i));
+    printf("%s\t%d\n", reg_name(i), gpr(i));
   }
 }
 
