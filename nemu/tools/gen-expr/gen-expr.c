1c6c68b158ef018478a2b6b93523707c82ddcd15
tracer-ics2024 <tracer@njuics.org>
2024-11-25 21:24:32 -0500

    >  compile NEMU 3019244266 lirunze Linux ubuntu-vm 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:32 up  3:22,  1 user,  load average: 0.36, 0.46, 0.35

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -31,8 +31,82 @@ static char *code_format =
 "  return 0; "
 "}";
 
+static inline int choose(int n) {
+  return rand() % n;
+}
+
+static inline void gen(char e) {
+  int i = 0;
+  while (buf[i] != '\0') i++;
+  buf[i] = e;
+  buf[i+1] = '\0';
+  return;
+}
+
+static inline void gen_num() {
+  uint32_t num = ((uint32_t) rand()) % 10000;
+  sprintf(buf+strlen(buf), "%d", num);
+  return;
+}
+
+static inline void gen_op() {
+  switch(choose(4)) {
+    case 0: gen('+'); break;
+    case 1: gen('-'); break;
+    case 2: gen('*'); break;
+    case 3: gen('/'); break;
+    default: break;
+  }
+}
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  // buf[0] = '\0';
+  char *s = buf;
+  if (strlen(buf) > 0 && *(buf+strlen(buf)-1) == '/') {
+    s = buf+strlen(buf);
+  }
+  int n = choose(3);
+  switch(n) {
+    case 0: gen_num(); break;
+    case 1: 
+      gen('(');
+      gen_rand_expr();
+      gen(')');
+      break;
+    default:
+      gen_rand_expr();
+      gen_op();
+      gen_rand_expr();
+      break;
+  }
+
+  // test division 0
+  if (s != buf) {
+    sprintf(code_buf, code_format, s);
+    FILE *fp = fopen("/tmp/.code.c", "w");
+    assert(fp != NULL);
+    fputs(code_buf, fp);
+    fclose(fp);
+
+    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    if(ret != 0){
+			printf("ret: %d\n",ret);
+		}
+		
+		fp = popen("/tmp/.expr", "r");
+		assert(fp != NULL);
+
+		int result;
+		fscanf(fp, "%d", &result);
+		pclose(fp);
+		
+		if(result == 0){
+			memset((void*)s, 0, strlen(s) * sizeof(char));
+			gen_rand_expr();
+		}
+
+  }
+
 }
 
 int main(int argc, char *argv[]) {
