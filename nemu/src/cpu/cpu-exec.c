75aa9df264703d182e24420c783df1e1ce3d95d4
tracer-ics2024 <tracer@njuics.org>
2024-11-27 00:44:57 -0500

    >  compile NEMU 3019244266 lirunze Linux ubuntu-vm 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:44:57 up  2:39,  1 user,  load average: 0.81, 0.75, 0.55

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -30,7 +30,7 @@ CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
-static bool CONFIG_WATCHPOINT = false;
+static bool CONFIG_WATCHPOINT = true;
 
 void device_update();
 
