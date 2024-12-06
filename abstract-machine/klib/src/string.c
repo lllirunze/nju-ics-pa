f8ee27dcdeedd7379931d79de19f9625e08d75c5
tracer-ics2024 <tracer@njuics.org>
2024-12-06 04:56:08 -0500

    >  compile NEMU 3019244266 lirunze Linux ubuntu-vm 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:56:08 up 32 min,  1 user,  load average: 0.01, 0.16, 0.30

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -41,9 +41,8 @@ int strcmp(const char *s1, const char *s2) {
   // panic("Not implemented");
   size_t i = 0;
   while (s1[i] != '\0' && s2[i] != '\0') {
-    if (s1[i] > s2[i]) return 1;
-    else if (s1[i] < s2[i]) return -1;
-    else i++;
+    if (s1[i] == s2[i]) i++;
+    else return s1[i] - s2[i];
   }
   if (s1[i] == '\0' && s2[i] == '\0') return 0;
   else if (s1[i] == '\0') return -1;
