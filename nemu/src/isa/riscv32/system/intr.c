6fdb26f95e4fc1a31fa4ecd4bb702c208e2f104b
lirunze <1927553272@qq.com>
2024-12-30 14:01:51 -0500

    NJU-ProjectN/nanos-lite ics2021 initialized
    NJU-ProjectN/nanos-lite 2a141760e31be246a7316942293a97873925bc2f Makefile: use header files in newlib-navy

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -16,7 +16,8 @@
 #include <isa.h>
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
-  /* TODO: Trigger an interrupt/exception with ``NO''.
+  /**
+   * TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
 
