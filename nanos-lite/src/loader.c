c4a1fa32b7cd7d283a08dea29de11c7147b22715
lirunze <1927553272@qq.com>
2025-01-05 16:26:16 -0500

    update loader but I don't implement the definition which is used to detect the ISA type in ELF file.

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -27,23 +27,21 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
    */
   // TODO();
 
-  Elf_Ehdr elf_header;
-  Elf_Phdr program_header;
-  ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
-  assert(memcmp(elf_header.e_ident, ELFMAG, SELFMAG) == 0);
-  assert(*((uint32_t *)&elf_header) == 0x464c457f);
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  // check valid elf
+  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
 
-  uint32_t i, phoff;
-  for (i=0; i<elf_header.e_phnum; i++) {
-    phoff = i*elf_header.e_phentsize + elf_header.e_phoff;
-    ramdisk_read(&program_header, phoff, sizeof(Elf_Phdr));
-    if (program_header.p_type == PT_LOAD) {
-      void *ptr_segment = (void *)program_header.p_vaddr;
-      ramdisk_read(ptr_segment, program_header.p_offset, program_header.p_filesz);
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    if (phdr[i].p_type == PT_LOAD) {
+      ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+      // set .bss with zeros
+      memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
     }
   }
-
-  return elf_header.e_entry;
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
