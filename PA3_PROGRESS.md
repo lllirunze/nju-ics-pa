482ddba95a8cd6c9669f6c6d0bab5c157db0aa28
lllirunze <lirunze.me@gmail.com>
2026-08-22 00:33:09 +0800

    implement nanos user program loading

--- PA3_PROGRESS.md
+++ PA3_PROGRESS.md
@@ -5,6 +5,6 @@
 - [x] 实现 `yield()` 的事件识别与返回。
 - [x] 实现选做的异常轨迹 etrace。
 - [x] 初始化 Nanos-lite 和 Navy-apps，并在 Nanos-lite 中处理 `EVENT_YIELD`。
-- [ ] 初始化并实现 Nanos-lite 的用户程序加载与系统调用。
+- [x] 初始化并实现 Nanos-lite 的用户程序加载与系统调用。
 - [ ] 实现 Nanos-lite 文件系统及设备文件。
 - [ ] 实现 PA3 后续应用运行所需的批处理系统功能。
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,32 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr ehdr;
+  (void)pcb;
+  (void)filename;
+
+  ramdisk_read(&ehdr, 0, sizeof(ehdr));
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
+  assert(ehdr.e_machine == EM_RISCV);
+  assert(ehdr.e_phentsize == sizeof(Elf_Phdr));
+
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    Elf_Phdr phdr;
+    ramdisk_read(&phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(phdr));
+    if (phdr.p_type != PT_LOAD) {
+      continue;
+    }
+
+    assert(phdr.p_memsz >= phdr.p_filesz);
+    ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
+    memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0,
+        phdr.p_memsz - phdr.p_filesz);
+  }
+
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
@@ -19,4 +42,3 @@ void naive_uload(PCB *pcb, const char *filename) {
   Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();
 }
-
