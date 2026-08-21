c1c128054ed014139e652a00d6ec75c1e8d2ab44
lllirunze <lirunze.me@gmail.com>
2026-08-21 23:38:37 +0800

    add device trace

--- PA2_PROGRESS.md
+++ PA2_PROGRESS.md
@@ -15,7 +15,7 @@
 - [x] 实现 `printf()`。
 - [x] 实现 `AM_TIMER_UPTIME`。
 - [x] 实现 `malloc()` 和 `free()`。
-- [ ] 实现 dtrace。
+- [x] 实现 dtrace。
 - [ ] 实现键盘 IOE（`AM_INPUT_KEYBRD`）。
 - [ ] 实现 VGA 的屏幕尺寸、同步与帧缓冲绘制 IOE。
 - [ ] 实现选做的声卡设备与音频 IOE。
--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -58,6 +58,8 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  IFDEF(CONFIG_DTRACE, log_write("dtrace: R %-8s addr = " FMT_PADDR " len = %d data = " FMT_WORD "\n",
+      map->name, addr, len, ret));
   return ret;
 }
 
@@ -67,4 +69,6 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+  IFDEF(CONFIG_DTRACE, log_write("dtrace: W %-8s addr = " FMT_PADDR " len = %d data = " FMT_WORD "\n",
+      map->name, addr, len, data));
 }
