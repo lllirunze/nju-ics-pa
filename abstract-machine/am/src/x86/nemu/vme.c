a2b9e09a0baa5cfdd3525a29cddcd23c9fa31422
lllirunze <lirunze.me@gmail.com>
2026-08-23 00:03:19 +0800

    implement x86 nemu virtual memory mapping

--- abstract-machine/am/src/x86/nemu/vme.c
+++ abstract-machine/am/src/x86/nemu/vme.c
@@ -2,6 +2,8 @@
 #include <nemu.h>
 #include <klib.h>
 
+#define SEG_KCODE 1
+
 static AddrSpace kas = {};
 static void* (*pgalloc_usr)(int) = NULL;
 static void (*pgfree_usr)(void*) = NULL;
@@ -18,6 +20,7 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   pgfree_usr = pgfree_f;
 
   kas.ptr = pgalloc_f(PGSIZE);
+  memset(kas.ptr, 0, PGSIZE);
 
   int i;
   for (i = 0; i < LENGTH(segments); i ++) {
@@ -36,6 +39,7 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
 
 void protect(AddrSpace *as) {
   PTE *updir = (PTE*)(pgalloc_usr(PGSIZE));
+  memset(updir, 0, PGSIZE);
   as->ptr = updir;
   as->area = USER_SPACE;
   as->pgsize = PGSIZE;
@@ -44,6 +48,14 @@ void protect(AddrSpace *as) {
 }
 
 void unprotect(AddrSpace *as) {
+  PTE *pdir = (PTE *)as->ptr;
+  for (int i = 0; i < 1024; i ++) {
+    if ((pdir[i] & (PTE_P | PTE_U)) == (PTE_P | PTE_U)) {
+      pgfree_usr((void *)(uintptr_t)(pdir[i] & ~0xfff));
+    }
+  }
+  pgfree_usr(pdir);
+  as->ptr = NULL;
 }
 
 void __am_get_cur_as(Context *c) {
@@ -57,8 +69,35 @@ void __am_switch(Context *c) {
 }
 
 void map(AddrSpace *as, void *va, void *pa, int prot) {
+  uintptr_t vaddr = (uintptr_t)va;
+  uintptr_t paddr = (uintptr_t)pa;
+  assert((vaddr & (PGSIZE - 1)) == 0);
+  assert((paddr & (PGSIZE - 1)) == 0);
+
+  PTE *pdir = (PTE *)as->ptr;
+  int pde = (vaddr >> 22) & 0x3ff;
+  int pte = (vaddr >> 12) & 0x3ff;
+  bool user = as != &kas;
+  if (!(pdir[pde] & PTE_P)) {
+    PTE *ptab = (PTE *)pgalloc_usr(PGSIZE);
+    memset(ptab, 0, PGSIZE);
+    pdir[pde] = (uintptr_t)ptab | PTE_P | PTE_W | (user ? PTE_U : 0);
+  }
+
+  PTE *ptab = (PTE *)(uintptr_t)(pdir[pde] & ~0xfff);
+  assert(!(ptab[pte] & PTE_P));
+  if (prot == MMAP_NONE) return;
+  ptab[pte] = paddr | PTE_P | (user ? PTE_U : 0) |
+      ((prot & MMAP_WRITE) ? PTE_W : 0);
 }
 
 Context* ucontext(AddrSpace *as, Area kstack, void *entry) {
-  return NULL;
+  Context *c = (Context *)((uintptr_t)kstack.end - sizeof(Context));
+  memset(c, 0, sizeof(*c));
+  c->eip = (uintptr_t)entry;
+  c->eflags = FL_IF;
+  c->cs = USEL(SEG_KCODE);
+  c->esp = (uintptr_t)as->area.end;
+  c->cr3 = as->ptr;
+  return c;
 }
